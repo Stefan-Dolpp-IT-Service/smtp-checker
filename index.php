d3a56c75bc54bebf318e8e13dac294cbcef9cdec
D-ITS <62642623+D-ITS@users.noreply.github.com>
2023-07-07 16:36:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if (isset($_POST['smtp'])) {
 <main class="form-signin">
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="pure-form pure-form-stacked">
         <fieldset>
-            <legend>D-ITS SMTP Checker</legend>
+            <legend>SMTP Checker</legend>
             <?php if(!empty($message)) { echo $message; } ?>
             <div class="pure-g">
                 <div class="pure-u-1 pure-u-md-1-3">
